0370c5133c42348d35c36fd2a6626c709a3e275f
Gadget Hackwrench <Gadget@HackwrenchLabs.com>
2023-12-20 15:18:12 -0800

    Added the copyright/license header to mkpgz.asm.

--- src/mkpgz.asm
+++ src/mkpgz.asm
@@ -1,3 +1,7 @@
+; WGET for the Foenix F256.
+; Copyright 2023 Jessie Oberreuter <Gadget@HackwrenchLabs.com>.
+; SPDX-License-Identifier: GPL-3.0-only
+
             .cpu    "65c02"
 
 *           = $8000-7
